12c85e0aa6f233e67d752d80a359d7995328226e
sagarThapa <thapasagar8089@gmail.com>
2021-03-26 11:38:24 +0545

    UI and UX design

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+    background-color: red;
+ 
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Kathmandu Branch</u></h3>
+            <p><span class="heading">Address - </span> Kathmandu Moden College, Kathmandu.</p>
+            <p><span class="heading">Tel - </span>+9779844318089</p>
+            <p><span class="heading">Email - </span>Kathmandu@onlinebank.com</p>
+            
+            
+            </div>
---
+++ footer.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tiyono</title>
+	<meta charset="utf-8">
+	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tiyono</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	
+	
+	<link href="./css/style.css" rel="stylesheet">
+
+	<title></title>
+</head>
+<body>
+<footer>
+    <div class="container footer-container">
+       <div class="row">
+       
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <ul class="contact">
+                         <span>Categories</span>    
+                         <a href="mens-products.php"><li>
+                            Mens
+                          </li>
+                        </a>
+                        
+                        <a href="womens-products.php">       
+                          <li>
+                            Womens
+                          </li>
+                          </a>
+                     </ul>
+                </div>
+                
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <ul class="contact">
+                         <span>Contact</span>    
+                         <li>
+                            <a href="#">Tiyono123@gmail.com</a>
+                          </li>
+                         <li>
+                           +9779844318089
+                          </li>      
+                         
+
+                    </ul>
+                </div>
+                
+                
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <ul class="contact">
+                         <span>Brands</span>    
+                         <li>
+                            GUCCI
+                          </li>
+                               
+                          <li>
+                             HERMES
+                          </li>
+                               
+                          <li>
+                            PRADA
+                          </li>
+                               
+                          <li>
+                            LOUIS VUITTON
+                          </li>
+                               
+                          <li>
+                            FENDI
+                         </li>
+                    </ul>
+                </div>
+           
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                   <ul class="contact">
+                              <span>NEED HELP?</span>    
+                              
+                               <li>
+                                    <a href="#">Track Order</a>
+                               </li>
+                                
+                               <li>
+                                    <a href="#">Returns Policy</a>
+                               </li>
+                               
+                               <li>
+                                    <a href="#">Cancellation</a>
+                               </li>
+                                
+                               <li>
+                                    <a href="#">Shipping Information</a>
+                              </li>
+                              <li>
+                                    <a href="#">FAQs</a>
+                              </li>
+                              <li>
+                                    <a href="#">Terms and Conditions</a>
+                              </li>
+                              <li>
+                                    <a href="#">Privacy Policy</a>
+                              </li>
+
+                              
+                     </ul>
+               </div>
+           </div>
+           </body>
+           </html>
